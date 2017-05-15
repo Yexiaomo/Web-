0d7f8fbbc8ffb548e4a5877c28dc97957ca00dcd
Yexiaomo <markdownpad@163.com>
2017-05-15 16:43:46 +0800

    修改 078

--- Web前端开发100例/078-HTML5 地理定位/js/index.js
+++ Web前端开发100例/078-HTML5 地理定位/js/index.js
@@ -1,4 +1,6 @@
-var x=document.getElementById("demo");
+function g(id) {
+  return document.getElementById(id);
+}
 function getLocation()
 {
   if (navigator.geolocation)
@@ -7,7 +9,7 @@ function getLocation()
   }
   else
   {
-    x.innerHTML="该浏览器不支持获取地理位置。";
+    g('demo').innerHTML="该浏览器不支持获取地理位置。";
   }
 }
 
@@ -25,16 +27,16 @@ function showError(error)
   switch(error.code) 
   {
     case error.PERMISSION_DENIED:
-      x.innerHTML="用户拒绝对获取地理位置的请求。"
+      g('demo').innerHTML="用户拒绝对获取地理位置的请求。"
       break;
     case error.POSITION_UNAVAILABLE:
-      x.innerHTML="位置信息是不可用的。"
+      g('demo').innerHTML="位置信息是不可用的。"
       break;
     case error.TIMEOUT:
-      x.innerHTML="请求用户地理位置超时。"
+      g('demo').innerHTML="请求用户地理位置超时。"
       break;
     case error.UNKNOWN_ERROR:
-      x.innerHTML="未知错误。"
+      g('demo').innerHTML="未知错误。"
       break;
   }
 }
