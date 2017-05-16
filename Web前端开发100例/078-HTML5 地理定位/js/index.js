5cd547d05e40c852fa6ab6481d10b8702dcabb03
Yexiaomo <markdownpad@163.com>
2017-05-16 11:22:34 +0800

    重写078案例
    将原来显示谷歌地图,改为显示经纬度

--- Web前端开发100例/078-HTML5 地理定位/js/index.js
+++ Web前端开发100例/078-HTML5 地理定位/js/index.js
@@ -1,42 +1,33 @@
+//通用函数
 function g(id) {
   return document.getElementById(id);
 }
-function getLocation()
-{
-  if (navigator.geolocation)
-  {
-    navigator.geolocation.getCurrentPosition(showPosition,showError);
-  }
-  else
-  {
-    g('demo').innerHTML="该浏览器不支持获取地理位置。";
+
+//获取地理位置
+function getLocation() {
+  g("demo").innerHTML="正在获取中...";
+
+  //检测浏览器是否支持
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+
+    //检测浏览器是否正确获取地理位置
+    setTimeout('showError()',10000);
+  } else {
+    showError();
   }
 }
 
-function showPosition(position)
-{
-  var latlon=position.coords.latitude+","+position.coords.longitude;
-
-  var img_url="http://maps.googleapis.com/maps/api/staticmap?center="
-  +latlon+"&zoom=14&size=400x300&sensor=false";
-  document.getElementById("mapholder").innerHTML="<img src='"+img_url+"'>";
+//显示位置
+function showPosition(position) {
+  g("demo").innerHTML="纬度: " + position.coords.latitude + 
+  "<br />经度: " + position.coords.longitude;
 }
 
-function showError(error)
-{
-  switch(error.code) 
-  {
-    case error.PERMISSION_DENIED:
-      g('demo').innerHTML="用户拒绝对获取地理位置的请求。"
-      break;
-    case error.POSITION_UNAVAILABLE:
-      g('demo').innerHTML="位置信息是不可用的。"
-      break;
-    case error.TIMEOUT:
-      g('demo').innerHTML="请求用户地理位置超时。"
-      break;
-    case error.UNKNOWN_ERROR:
-      g('demo').innerHTML="未知错误。"
-      break;
+//-->浏览器不支持
+//-->浏览器获取地理位置失败,延迟10秒后执行
+function showError() {
+  if (g('demo').innerHTML === "正在获取中...") {
+    g("demo").innerHTML="获取地理位置失败,请检查浏览器、网络等是否出现问题";
   }
 }
