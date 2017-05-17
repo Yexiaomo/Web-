19ac5909a3d11d30ec15996a1364c8290569bb9e
Yexiaomo <markdownpad@163.com>
2017-05-17 11:06:30 +0800

    修改 089 JS

--- Web前端开发100例/089-Ajax随机产生一条名人名言/js/index.js
+++ Web前端开发100例/089-Ajax随机产生一条名人名言/js/index.js
@@ -1,10 +1,3 @@
-function inIframe () {
-  try {
-    return window.self !== window.top;
-  } catch (e) { 
-    return true; 
-  }
-}
 //定义网页背景颜色列表
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
 //获得的名言 和 作者 的变量
@@ -26,14 +19,7 @@ function getQuote(){
     },
     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
     success: function(response) {
-      var r = JSON.parse(response);
-      currentQuote = r.quote;
-      currentAuthor = r.author;
-      if(inIframe())
-      {
-        $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
-        $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote));
-      }
+      // 将获取到的文本显示在页面内
       $(".quote-text").animate({
           opacity: 0
         }, 500,
@@ -41,9 +27,9 @@ function getQuote(){
           $(this).animate({
             opacity: 1
           }, 500);
-          $('#text').text(r.quote);
+          $('#text').text(response.quote);
         });
-
+      // 将获取到的文本显示在页面内
       $(".quote-author").animate({
           opacity: 0
         }, 500,
@@ -51,9 +37,9 @@ function getQuote(){
           $(this).animate({
             opacity: 1
           }, 500);
-          $('#author').html(r.author);
+          $('#author').html(response.author);
         });
-
+      // 设定一种颜色
       var color = Math.floor(Math.random() * colors.length);
       $("html body").css('background-color',colors[color]);
       $(".button").css('background-color',colors[color]);
